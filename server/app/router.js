b2a86007c818499a85a3989ca8077c4b280b9448
unknown <i-zhutongtong@360.cn>
2019-12-20 18:51:16 +0800

    '更新server-config'

--- server/app/router.js
+++ server/app/router.js
@@ -4,7 +4,8 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = async (app) => {
-  const result = await app.mysql.insert('posts', { title: '1Hello World tongtonga' });
+  const result = await app.mysql.insert('posts', { title: '1Hello World tongtong' });
+  console.log('app', result)
   const { router, controller } = app;
   router.get('/', controller.home.index);
 };
