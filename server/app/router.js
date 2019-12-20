94316a6ff54fb4f6f80de13e345e47bb397cad7a
unknown <i-zhutongtong@360.cn>
2019-12-20 17:27:53 +0800

    '使用egg连接远程数据库'

--- server/app/router.js
+++ server/app/router.js
@@ -3,7 +3,8 @@
 /**
  * @param {Egg.Application} app - egg application
  */
-module.exports = app => {
+module.exports = async (app) => {
+  const result = await app.mysql.insert('posts', { title: '1Hello World tongtonga' });
   const { router, controller } = app;
   router.get('/', controller.home.index);
 };
